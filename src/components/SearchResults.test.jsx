f4ce5b6ae06f62250d03a9ad7cc5fd3add55ce1e
Devlin Glasman <devlin.glasman@gmail.com>
2018-11-08 15:54:13 +0000

    Corrects tests

--- src/components/SearchResults.test.jsx
+++ src/components/SearchResults.test.jsx
@@ -6,50 +6,31 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({adapter: new Adapter()});
 
-it('should render an <li> element for every pokemon in props', () => {
-  const fakeResults = [
-    {
-      url: 'http://pokeapi.salestock.net/api/v2/pokemon/1/',
-      name: 'bulbasaur',
-    },
-    {
-      url: 'http://pokeapi.salestock.net/api/v2/pokemon/2/',
-      name: 'ivysaur',
-    },
-    {
-      url: 'http://pokeapi.salestock.net/api/v2/pokemon/3/',
-      name: 'venusaur',
-    },
-    {
-      url: 'http://pokeapi.salestock.net/api/v2/pokemon/4/',
-      name: 'charmander',
-    },
+let searchResultsNames;
+
+beforeEach(() => {
+  searchResultsNames = [
+    'bulbasaur',
+    'ivysaur',
+    'venusaur',
+    'oddish',
+    'gloom',
+    'vileplume',
   ];
-  const wrapper = Enzyme.shallow(<SearchResults testResults={fakeResults} />);
-  expect(wrapper.find('li').length).toEqual(fakeResults.length);
+});
+
+it('should render an <li> element for every pokemon in props', () => {
+  const wrapper = Enzyme.shallow(
+    <SearchResults resultsPokeNames={searchResultsNames} />,
+  );
+  expect(wrapper.find('li').length).toEqual(searchResultsNames.length);
   4;
 });
 
 it('should display the Pokemon name for each <li> element, capitalised', () => {
-  const fakeResults = [
-    {
-      url: 'http://pokeapi.salestock.net/api/v2/pokemon/1/',
-      name: 'bulbasaur',
-    },
-    {
-      url: 'http://pokeapi.salestock.net/api/v2/pokemon/2/',
-      name: 'ivysaur',
-    },
-    {
-      url: 'http://pokeapi.salestock.net/api/v2/pokemon/3/',
-      name: 'venusaur',
-    },
-    {
-      url: 'http://pokeapi.salestock.net/api/v2/pokemon/4/',
-      name: 'charmander',
-    },
-  ];
-  const wrapper = Enzyme.shallow(<SearchResults testResults={fakeResults} />);
+  const wrapper = Enzyme.shallow(
+    <SearchResults resultsPokeNames={searchResultsNames} />,
+  );
   const firstElement = wrapper.find('li').first();
-  expect(firstElement.contains("Bulbasaur")).toEqual(true);
+  expect(firstElement.contains('Bulbasaur')).toEqual(true);
 });
